2a20ab9609edff0e91a1aa2dc8dd439d69715426
Bunduchi  Ion <ibunduche@gmail.com>
2022-03-16 12:31:03 +0000

    M1 3

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView
+from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView, AddBlogView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -10,5 +10,6 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('add-blog/', AddBlogView.as_view(), name='add_blog'),
     path('blog/<int:pk>/', BlogItemView.as_view(), name='blog_item'),
 ]
